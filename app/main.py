60f8b58bd542b468ed6bb59cc5d4ce5e3a185650
Rabin Dhamala <rabinkmc@gmail.com>
2023-11-17 13:15:20 +0545

    use regular expression

--- app/main.py
+++ app/main.py
@@ -1,12 +1,12 @@
-# Uncomment this to pass the first stage
 import socket
+import re
 
 
 def main():
     print("Starting server:")
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
 
-    def get_path(data):
+    def get_path(data) -> str:
         request = data.decode().split("\r\n")
         head = request[0]
         path = head.split(" ")[1]
@@ -16,8 +16,15 @@ def main():
     data = sock.recv(128)
 
     path = get_path(data)
-    echo = path.split("/")[-1]
-    response = f"HTTP/1.1 200 OK\r\n\r\nContent-Type: text/plain\r\n{echo}\r\n"
+    regex = re.compile(r"/echo/(.+$)")
+    match = regex.match(path)
+    if not match:
+        sock.send(b"HTTP/1.1 404 Not Found\r\n\r\n")
+        sock.close()
+        return
+
+    param = match.group(1)
+    response = f"HTTP/1.1 200 OK\r\n\r\nContent-Type: text/plain\r\n{param}\r\n"
     sock.send(response.encode())
     sock.close()
 
