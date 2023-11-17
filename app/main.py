9f60cc2f0803512f57f498472d7c1d9d0df16b49
Rabin Dhamala <rabinkmc@gmail.com>
2023-11-17 11:51:19 +0545

    respond with content

--- app/main.py
+++ app/main.py
@@ -5,8 +5,6 @@ import socket
 def main():
     print("Starting server:")
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    sock, _ = server_socket.accept()
-    data = sock.recv(128)
 
     def get_path(data):
         request = data.decode().split("\r\n")
@@ -14,11 +12,14 @@ def main():
         path = head.split(" ")[1]
         return path
 
+    sock, _ = server_socket.accept()
+    data = sock.recv(128)
+
     path = get_path(data)
-    if path == "/":
-        sock.send(b"HTTP/1.1 200 OK\r\n\r\n")
-    else:
-        sock.send(b"HTTP/1.1 404 Not Found\r\n\r\n")
+    echo = path.split("/")[-1]
+    response = f"HTTP/1.1 200 OK\r\n\r\nContent-Type: text/plain\r\n{echo}\r\n"
+    sock.send(response.encode())
+    sock.close()
 
 
 if __name__ == "__main__":
