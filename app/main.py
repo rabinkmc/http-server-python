4ee3ad31a7d9fce63daa28f48066fe249967a18d
Rabin Dhamala <rabinkmc@gmail.com>
2023-11-17 11:17:08 +0545

    200 for "/" else 404

--- app/main.py
+++ app/main.py
@@ -3,17 +3,22 @@ import socket
 
 
 def main():
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
-    print("Logs from your program will appear here!")
-
-    # Uncomment this to pass the first stage
-
+    print("Starting server:")
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    sock, _ = server_socket.accept()  # wait for client
-    # server_socket.send(b"HTTP/1.1 200 OK\r\n\r\n")
+    sock, _ = server_socket.accept()
     data = sock.recv(128)
-    print(data)
-    sock.send(b"HTTP/1.1 200 OK\r\n\r\n")
+
+    def get_path(data):
+        request = data.decode().split("\r\n")
+        head = request[0]
+        path = head.split(" ")[1]
+        return path
+
+    path = get_path(data)
+    if path == "/":
+        sock.send(b"HTTP/1.1 200 OK\r\n\r\n")
+    else:
+        sock.send(b"HTTP/1.1 404 Not Found\r\n\r\n")
 
 
 if __name__ == "__main__":
