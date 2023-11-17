8d4cda256bccbf1b7f4f5eff482be4f81af6f55f
Rabin Dhamala <rabinkmc@gmail.com>
2023-11-17 13:25:44 +0545

    send 200 status on /

--- app/main.py
+++ app/main.py
@@ -2,31 +2,45 @@ import socket
 import re
 
 
+def get_path(data) -> str:
+    request = data.decode().split("\r\n")
+    head = request[0]
+    path = head.split(" ")[1]
+    return path
+
+
+STATUS_200 = "HTTP/1.1 200 OK\r\n\r\n"
+STATUS_404 = "HTTP/1.1 404 Not Found\r\n\r\n"
+
+
 def main():
     print("Starting server:")
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-
-    def get_path(data) -> str:
-        request = data.decode().split("\r\n")
-        head = request[0]
-        path = head.split(" ")[1]
-        return path
 
     sock, _ = server_socket.accept()
     data = sock.recv(128)
 
     path = get_path(data)
-    regex = re.compile(r"/echo/(.+$)")
-    match = regex.match(path)
-    if not match:
-        sock.send(b"HTTP/1.1 404 Not Found\r\n\r\n")
+
+    if path == "/":
+        response = STATUS_200
+        sock.send(response.encode())
         sock.close()
         return
 
-    param = match.group(1)
-    response = f"HTTP/1.1 200 OK\r\n\r\nContent-Type: text/plain\r\n{param}\r\n"
-    sock.send(response.encode())
-    sock.close()
+    regex = re.compile(r"/echo/(.+$)")
+    match = regex.match(path)
+    if not match:
+        response = STATUS_404
+        sock.send(response.encode())
+        sock.close()
+    else:
+        param = match.group(1)
+        body = f"Content-Type: text/plain\r\n{param}\r\n"
+        response = STATUS_200 + body
+        sock.send(response.encode())
+        sock.close()
+        return
 
 
 if __name__ == "__main__":
