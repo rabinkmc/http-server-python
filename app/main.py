c781a9f16e5f5c288035f3a6ebec3585abcca03f
Rabin Dhamala <rabinkmc@gmail.com>
2023-11-19 10:04:09 +0545

    handle concurrent connection

--- app/main.py
+++ app/main.py
@@ -1,29 +1,20 @@
 import socket
 import re
-
-
-def get_path(data) -> str:
-    request = data.decode().split("\r\n")
-    head = request[0]
-    path = head.split(" ")[1]
-    return path
-
+from threading import Thread
+from time import sleep
 
 STATUS_200 = "HTTP/1.1 200 OK\r\n"
 STATUS_404 = "HTTP/1.1 404 Not Found\r\n\r\n"
 
 
-def main():
-    print("Starting server:")
-    server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-
-    sock, _ = server_socket.accept()
+def handle_request(sock):
+    global counter
     data = sock.recv(128)
-
     path = get_path(data)
 
+    headers = data.decode().strip("\r\n").split("\r\n")[1:]
+    print("start of thread")
     if path == "/user-agent":
-        headers = data.decode().strip("\r\n").split("\r\n")[1:]
         agent = ""
         for header in headers:
             if header.startswith("User-Agent"):
@@ -36,23 +27,36 @@ def main():
             f"Content-Length: {len(agent)}\r\n\r\n"
             f"{agent}\r\n"
         )
-        sock.send(response.encode())
-
     elif path == "/":
         response = STATUS_200 + "\r\n"
-        sock.send(response.encode())
-
     elif match := re.match(r"/echo/(.+$)", path):
         param = match.group(1)
         content_type = "Content-Type: text/plain\r\n"
         content_length = f"Content-Length: {len(param)}\r\n\r\n"
         content = f"{param}\r\n"
         response = STATUS_200 + content_type + content_length + content
-        sock.send(response.encode())
     else:
         response = STATUS_404
-        sock.send(response.encode())
+
+    sock.send(response.encode())
     sock.close()
+
+
+def get_path(data) -> str:
+    request = data.decode().split("\r\n")
+    head = request[0]
+    path = head.split(" ")[1]
+    return path
+
+
+def main():
+    print("Starting server:")
+    server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
+
+    while True:
+        sock, _ = server_socket.accept()
+        new_thread = Thread(target=handle_request, args=(sock,))
+        new_thread.start()
 
 
 if __name__ == "__main__":
